2db2862580aae46e47a2279da1159120622cf053
Sarthak Sharma <sarthaksharma2199@outlook.com>
2020-11-13 19:12:02 +0530

    modified data

--- generator.js
+++ generator.js
@@ -10,7 +10,7 @@ for(i=0; i<num_of_entries; i++){
     author_name=faker.name.findName();
     post_title=faker.lorem.sentence();
     post_body=faker.lorem.paragraph();
-    post_image=faker.image.abstract(); 
+    post_image=faker.image.business(); 
     posts.data.push({author_name: author_name, post_title: post_title, post_body: post_body, post_image:post_image});
 }
 
